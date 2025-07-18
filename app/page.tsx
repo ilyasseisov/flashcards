54a1bfabef16eee68c0987ae224ceac3b84f714c
Ilyas Seisov <ilyasseisov@gmail.com>
2025-07-18 13:20:08 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="space-x-0 text-4xl">
+      <h1 className="font-normal">Hello</h1>
+      <Button size="lg" variant="default">
+        Create account
+      </Button>
+    </main>
+  );
+}
---
+++ constants/routes.ts
@@ -0,0 +1,11 @@
+const ROUTES = {
+  // HOME: '/',
+  // BLOG: '/blog',
+  // POST: (slug: string) => `/blog/${slug}`,
+};
+
+export default ROUTES;
+
+// usage
+// <Link href={ROUTES.HOME}>
+// <Link href={ROUTES.POST(post.slug)}>
