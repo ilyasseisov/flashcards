8d151fbdc0917ed1645164f430a1d7a614bc1cfe
Ilyas Seisov <ilyasseisov@gmail.com>
2025-07-21 12:56:52 +0500

    fix: adjust column spans for LeftSidebar and main content layout

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -9,10 +9,10 @@ const Page = () => {
   return (
     <>
       <main className="grid grid-cols-12 gap-4">
-        <section className="col-span-3">
+        <section className="col-span-6 bg-amber-50 lg:col-span-5 xl:col-span-4">
           <LeftSidebar />
         </section>
-        <section className="col-span-9"></section>
+        <section className="col-span-6 bg-blue-50 lg:col-span-7 xl:col-span-8"></section>
       </main>
     </>
   );
