dcb827b1ba73b0d9e676985150e82a1585ca65ef
Ilyas Seisov <ilyasseisov@gmail.com>
2025-07-24 13:49:51 +0500

    feat: refactor flashcard page and add MongoDB connection with Mongoose

--- app/flashcards/[category]/[subcategory]/page.tsx
+++ app/flashcards/[category]/[subcategory]/page.tsx
@@ -1,7 +1,3 @@
-import { FlashcardQuiz } from "@/components/quiz/flashcard-quiz";
-import { getFlashcardSet } from "@/lib/flashcard-data";
-import { notFound } from "next/navigation";
-
 interface PageProps {
   params: {
     category: string;
@@ -12,18 +8,11 @@ interface PageProps {
 export default async function Page({ params }: PageProps) {
   const { category, subcategory } = await params;
 
-  const flashcardSet = getFlashcardSet(category, subcategory);
-
-  if (!flashcardSet) {
-    notFound();
-  }
-
   return (
     <main className="container py-8">
-      <h1 className="mb-8 bg-amber-50 text-center text-2xl font-bold">
-        {flashcardSet.title}
-      </h1>
-      <FlashcardQuiz flashcards={flashcardSet.flashcards} />
+      <h1>Quiz</h1>
+      <p>Category: {category}</p>
+      <p>Subcategory: {subcategory}</p>
     </main>
   );
 }
